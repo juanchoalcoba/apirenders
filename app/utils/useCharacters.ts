798495459c700d99ff8d9f59221071c9849483f5
juanchoalcoba <juanchoalcoba@gmail.com>
2025-11-15 19:25:35 -0300

    update

--- app/utils/useCharacters.ts
+++ app/utils/useCharacters.ts
@@ -2,22 +2,22 @@
 import { Character, ApiResponse } from "../types/types";
 
 export async function getCharacters(): Promise<Character[]> {
+  // 🔹 Fetch desde la API de Rick and Morty
   const res = await fetch("https://rickandmortyapi.com/api/character", {
-    cache: "no-store",
+    cache: "no-store", // 🔹 Siempre datos frescos
   });
 
   const data: ApiResponse = await res.json();
+  console.log(data.results);
+  // 🔹 Tipamos la respuesta completa para TS
 
-  const characters: Character[] = data.results.map((c) => ({
-    id: c.id,
-    name: c.name,
-    status: c.status,
-    species: c.species,
-    gender: c.gender,
-    image: c.image,
-    origin: c.origin,
-    location: c.location,
-  }));
+  // 🔹 Transformamos la respuesta a nuestro Character simplificado
+  const characters: Character[] = data.results.map(
+    (c) =>
+      ({
+        ...c,
+      } as Character)
+  );
 
-  return characters;
+  return characters; // 🔹 Retornamos solo los datos que necesitamos
 }
